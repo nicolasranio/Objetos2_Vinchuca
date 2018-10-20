5e270672f38223d52f873e17c322bd657f5deab3
Nicolas Rano <nicolasranio@gmail.com>
2018-10-19 23:24:27 -0300

    commit inicial del proyecto

---
+++ test/test/TestMuestra.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class TestMuestra {
+	
+	Mock muestra;
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void test_creacion_muestra() {
+		
+	}
+	
+	
+}
